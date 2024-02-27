f72c19250de9931ea9d78be267b5e38dde50f4cf
Karel Tropp <koder@kareltropp.cz>
2024-02-27 13:19:39 +0100

    Big Bang

---
+++ Img.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace ktropp\yii2_webp;
+
+use yii\base\Widget;
+use yii\helpers\Html;
+
+class Img extends Widget
+{
+    public $src;
+
+    public $lazy = false;
+
+    public $options = [];
+
+    private $_webp;
+
+    public function init()
+    {
+        parent::init();
+        $this->_webp = Webp::get($this->src);
+    }
+
+    public function run()
+    {
+        $srcset = 'srcset';
+        $src = 'src';
+        $class = [];
+        if($this->lazy){
+            $srcset = 'data-srcset';
+            $src = 'data-src';
+            $class[] = 'lazy';
+        }
+        $return = Html::beginTag('picture');
+        if (!empty($this->_webp)) {
+            $return .= Html::tag('source', '', [$srcset => $this->_webp, 'type' => 'image/webp']);
+        }
+        $return .= Html::tag('img', '', [$src => $this->src, 'class' => $class]);
+        $return .= Html::endTag('picture');
+
+        return $return;
+    }
+}
---
+++ Webp.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ktropp\yii2_webp;
+
+use Yii;
+use WebPConvert\WebPConvert;
+
+class Webp
+{
+    private static function createWebp($source, $destination)
+    {
+        WebPConvert::Convert($source, $destination, []);
+    }
+
+    /*
+     * Get Webp path from original img file
+     */
+    private static function getWebpPath($img)
+    {
+        $fileInfo = pathinfo($img);
+
+        return $fileInfo['dirname'] . '/' . $fileInfo['filename'] . '.webp';
+    }
+
+    /*
+     * Get path to webp file
+     * @param string $img
+     */
+    public static function get($img)
+    {
+        $imgFullPath = Yii::getAlias('@webroot') . $img;
+        $webpFilePath = static::getWebpPath($imgFullPath);
+        $webpFileUrl = static::getWebpPath($img);
+
+        if (!is_file($imgFullPath))
+            return false;
+
+        if(file_exists($webpFilePath))
+            return (filesize($webpFilePath) < filesize($imgFullPath) ? $webpFileUrl : false);
+
+        static::createWebp($imgFullPath, $webpFilePath);
+
+        return $webpFileUrl;
+    }
+}

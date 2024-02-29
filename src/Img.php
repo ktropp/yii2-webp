62a0c294af8ed920376ba8969d217d822e1276da
Karel Tropp <koder@kareltropp.cz>
2024-02-29 08:54:15 +0100

    img width and height

--- src/Img.php
+++ src/Img.php
@@ -4,6 +4,7 @@ namespace ktropp\yii2webp;
 
 use yii\base\Widget;
 use yii\helpers\Html;
+use Yii;
 
 class Img extends Widget
 {
@@ -31,11 +32,18 @@ class Img extends Widget
             $src = 'data-src';
             $class[] = 'lazy';
         }
+        $imgSize = [];
+        if(is_file(Yii::getAlias('@webroot') . $this->src))
+            $size = @getimagesize(Yii::getAlias('@webroot') . $this->src);
+        if(isset($size[0]))
+            $imgSize['width'] = $size[0];
+        if(isset($size[1]))
+            $imgSize['height'] = $size[1];
         $return = Html::beginTag('picture');
         if (!empty($this->_webp)) {
             $return .= Html::tag('source', '', [$srcset => $this->_webp, 'type' => 'image/webp']);
         }
-        $return .= Html::tag('img', '', array_merge([$src => $this->src, 'class' => $class], $this->options));
+        $return .= Html::tag('img', '', array_merge([$src => $this->src, 'class' => $class], $imgSize, $this->options));
         $return .= Html::endTag('picture');
 
         return $return;
